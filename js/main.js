7258fc60aebee193553520b0d20d31c13e3b3ff8
TicaTac <lior.keshet@gmail.com>
2016-07-12 18:40:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿
+function myfunction(text) {
+
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=telaviv&unit=metric&appid=a69961c7031783d91d299c95e15920da", function (data) {
+       alert(text);
+    //    debugger;
+
+    });
+}
